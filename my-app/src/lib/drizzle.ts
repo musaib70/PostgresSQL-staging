338563933fd7ea3a59b13f3f6e07ff506e3df6aa
Musaib70 <musaibkhan62@yahoo.com>
2023-05-18 21:00:28 +0500

    drizzle orm

---
+++ my-app/src/lib/drizzle.ts
@@ -0,0 +1,26 @@
+import {
+    pgTable,
+    serial,
+    text,
+    varchar,
+    timestamp,
+    boolean,
+  } from "drizzle-orm/pg-core";
+  import { drizzle } from "drizzle-orm/vercel-postgres";
+  import { InferModel } from "drizzle-orm";
+  import { sql } from "@vercel/postgres";
+import { unique } from "next/dist/build/utils";
+import { table } from "console";
+  
+  export const todoTable = pgTable("todo", {
+    id: serial("id").primaryKey().notNull(),
+    task: varchar("task", {length:50}).notNull(),
+    description: varchar("description", {length: 500}),
+    
+  });
+  
+  
+  export type Todo = InferModel<typeof todoTable>;
+  export type NewTodo = InferModel<typeof todoTable, "insert">; // insert type
+  
+  export const db = drizzle(sql);
